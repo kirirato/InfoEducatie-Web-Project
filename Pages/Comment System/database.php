fd0dfd31dbc2d6ec9c3a7cc0b50e01b1e689f6ae
kirirato <opriaionut14@gmail.com>
2016-06-05 00:49:25 +0300

    Delete database.php

--- Pages/Comment System/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$dbhost = 'localhost';
-	$dbuser = 'root';
-	$dbpass = '';
-	$dbname = 'local';
-	
-	$connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die(mysqli_error($connect));
-?>
